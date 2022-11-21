8d9cfd4553a6ad7427a8d061094e23c522e84164
Jorge Besnier <jorgebesnierb@outlook.com>
2022-11-20 21:35:59 -0600

    responsive nav bar, and main page with images and titles

--- my-portfolio/src/routes/Home.js
+++ my-portfolio/src/routes/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg/>
     </div>
+    
   )
 }
 
